8fb218a8de8a4ebddd36f8267f60dc75d72d4866
Schuyler Thompson <sdt16@case.edu>
2013-01-31 19:09:20 +0000

    doing assignment 3.1

--- number_to_word.c
+++ number_to_word.c
@@ -1,19 +1,31 @@
+#include <string.h>
+#include <stdio.h>
 #include "number_to_word.h"
 
 void print_words_for_amount(int dollars, int cents) 
 {
+	char* words_for_numbers[] = {"", "one ", "two ", "three ", "four ", "five ", "six ", 
+		"seven ", "eight ", "nine "};
+	char* words_for_teens[] = {"ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", 
+		"fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "};
+	char* words_for_tens[] = {"", "ten ", "twenty ", "thirty ", "fourty ", "fifty ", "sixty ",
+		"sevety ", "eighty ", "ninty "};
+	printf("$%d.%02d -> ", dollars, cents);
 	char hundreds_string[50];
 	int hundreds = dollars/100;
+	hundreds_string[0] = '\0';
 	if (hundreds) {
 		strcat(hundreds_string, words_for_numbers[hundreds]);
-		strcat(hundreds_string, " hundred");
+		strcat(hundreds_string, "hundred ");
 	}
 
 	int tens = dollars/10%10;
 	if (tens == 1) {
-		printf("%s %s", hundreds_string, words_for_teens[dollars%10]);
+		printf("%s%s", hundreds_string, words_for_teens[dollars%10]);
 	} else {
-		printf("%s %s %s", hundreds_string, words_for_tens[tens], words_for_numbers[dollars%10]);
+		printf("%s%s%s", hundreds_string, words_for_tens[tens], dollars>0?words_for_numbers[dollars%10]:"zero ");
 	}
+
+	printf("and %d/100 dollars\n", cents);
 
 }
