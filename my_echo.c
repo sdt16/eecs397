8b1f06ef3f5e4f930cc462c16495742ca3272194
Schuyler Thompson <sdt16@case.edu>
2013-02-24 16:52:44 +0000

    trying to fix

--- my_echo.c
+++ my_echo.c
@@ -14,11 +14,12 @@ int has_n(int argc, char* argv[])
   return 1;
 }
 
-int get_r(int argc, char* argv[])
+int get_r(int argc, char* argv[], int* has_r)
 {
   if (argc > 2) {
     for (int i = 1; i<4; i++) {
       if (strcmp(argv[i], "-r") == 0) {
+	*has_r = 1;
 	return atoi(argv[i+1]);
       }
     }
@@ -26,20 +27,38 @@ int get_r(int argc, char* argv[])
   return 1;
 }
 
+int get_argc_offset(int n, int has_r)
+{
+  int offset = 0;
+  if (!n) {
+    offset++;
+  }
+  if (has_r) {
+    offset += 2;
+  }
+  return offset;
+}
+
 int main(int argc, char* argv[]) 
 {
+  int has_r = 0;
   int n = has_n(argc, argv);
-  int r = get_r(argc, argv);
+  int r = get_r(argc, argv, &has_r);
+  
+  int offset = get_argc_offset(n, has_r);
   
   fprintf(stderr, "n: %d\n", n);
   fprintf(stderr, "r: %d\n", r);
-  for (int i = 0; i < r; i++) {
-    printf("%s", argv[argc-1]);
-  }
-  if (n) {
-    printf("\n");
-  }
 
+  for (int i = 0; i < r; i++) {
+    for (int argc_pos = offset+1; argc_pos < argc; argc_pos++) {
+      printf("%s", argv[argc_pos]);
+    }
+    if (n) {
+      printf("\n");
+    }
+  }
+  
   return 0;
 }
   
