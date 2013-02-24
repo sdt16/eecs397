227e898ce5db7e46b2272b5d51437ea83a0b2a22
Schuyler Thompson <sdt16@case.edu>
2013-02-24 17:07:12 +0000

    fixed

--- my_echo.c
+++ my_echo.c
@@ -2,10 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 
+int get_max_argv(int argc) {
+  if (argc<4) {
+    return argc;
+  } else {
+    return 4;
+  }
+}
+
+
 int has_n(int argc, char* argv[]) 
 {
+  int max = get_max_argv(argc);
   if (argc > 2) {
-    for (int i = 1; i<4; i++) {
+    for (int i = 1; i<max; i++) {
       if (strcmp(argv[i], "-n") == 0) {
 	return 0;
       }
@@ -16,8 +26,9 @@ int has_n(int argc, char* argv[])
 
 int get_r(int argc, char* argv[], int* has_r)
 {
+  int max = get_max_argv(argc);
   if (argc > 2) {
-    for (int i = 1; i<4; i++) {
+    for (int i = 1; i<max; i++) {
       if (strcmp(argv[i], "-r") == 0) {
 	*has_r = 1;
 	return atoi(argv[i+1]);
@@ -53,6 +64,9 @@ int main(int argc, char* argv[])
   for (int i = 0; i < r; i++) {
     for (int argc_pos = offset+1; argc_pos < argc; argc_pos++) {
       printf("%s", argv[argc_pos]);
+      if (!(argc_pos == argc-1)) {
+	printf(" ");
+      }
     }
     if (n) {
       printf("\n");
